a89bb0671634a1dc403ce2c3731eb902f178154d
anandman03 <anandmansimar@gmail.com>
2020-10-21 15:33:38 +0530

    removed bugs in input

--- src/handleEvents.js
+++ src/handleEvents.js
@@ -27,41 +27,6 @@ const createNote = async (note) => {
     storage.storeItem(structuredNote);
 };
 
-
-const structureItem = async (item, type) => {
-    let newItem = new Object();
-    const length = Object.keys(item).length;
-
-    if(type === "NOTE") {
-        newItem = new Note();
-    }
-    else if(type === "TASK") {
-        newItem = new Task();
-    }
-    newItem._description = item[0];
-
-    const TL = (storage.FileExist()) ? require(pathConfig.filePath) : [];
-    newItem._id = TL.filter(ob => ob._type === type).length + 1;
-
-    if(length == 2) {
-        if(validator.Board(item[1])) {
-            newItem._board = item[1].substring(2);
-        }
-        else if(validator.Priority(item[1])) {
-            newItem._priority = parseInt(item[1].substring(2));
-        }
-    }
-    else if(length == 3) {
-        if(validator.Board(item[1])) {
-            newItem._board = item[1].substring(2);
-        }
-        if(validator.Priority(item[2])) {
-            newItem._priority = parseInt(item[2].substring(2));
-        }
-    }
-    return newItem;
-};
-
 const displayBoards = () => {
     const taskListner = (storage.FileExist()) ? require(pathConfig.filePath) : [];
     if(taskListner.length === 0) {
@@ -88,5 +53,73 @@ const displayBoards = () => {
     }
 };
 
+
+const structureItem = async (item, type) => {
+    let newItem = new Object();
+    if(type === "NOTE") {
+        newItem = new Note();
+    }
+    else if(type === "TASK") {
+        newItem = new Task();
+    }
+
+    const TL = (storage.FileExist()) ? require(pathConfig.filePath) : [];
+    newItem._id = TL.filter(ob => ob._type === type).length + 1;
+ 
+    const desc = await getDescription(item);
+    if(desc.length === 0) {
+        messages.Invalid();
+        return;
+    }
+    newItem._description = desc;
+
+    const db = await getDashboard(item);
+    if(db !== null) {
+        newItem._board = db;   
+    }
+
+    const priority = await getPriority(item);
+    if(priority !== null) {
+        newItem._priority = priority;
+    }
+
+    return newItem;
+};
+
+const getDescription = async (item) => {
+    let description = new Array();
+    for(const str of item) {
+        if((str[0] === 'p' || str[0] === 'b') && str[1] === ':') {
+            break;
+        }
+        description.push(str);
+    }
+    return description.join(' ').trim();
+};
+
+const getDashboard = async (item) => {
+    const str = item.join(' ');
+    let index = str.indexOf("b:");
+    if(index === -1) {
+        return null;
+    }
+    let board = new String();
+    for(let i = index + 2 ; i < str.length ; i++) {
+        if(str[i] === 'p' && str[i+1] === ':') {
+            break;
+        }
+        board += str[i];
+    }
+    return board.trim();
+};
+
+const getPriority = async (item) => {
+    for(const str of item) {
+        if(str.substring(2) === "p:") {
+            return str[2].trim();
+        }
+    }
+    return null;
+};
 
 module.exports = { createTask, createNote, displayBoards };
