08fe1a20c29d1b51f5daf4d4b8b6d7396d2fcf36
anandman03 <anandmansimar@gmail.com>
2020-10-22 20:05:29 +0530

    improved find query logs

--- src/viewBoard.js
+++ src/viewBoard.js
@@ -7,24 +7,37 @@ const validator = require("./validator");
 const getDate = require("./helpers/getDate");
 
 
+let done = 0;
+let pending = 0;
+let notes = 0;
+
+const initialise = () => {
+    done = pending = notes = 0;
+};
+
+const calculate = () => {
+    let percentCompletion = Math.floor(100*(done/(done + pending)));
+    messages.taskCompleteData(percentCompletion);
+    messages.overView(done, pending, notes);
+    messages.newLine();
+};
+
 const findItem = async (item) => {
     const value = item.join(' ').trim();
 
+    messages.newLine();
     if(validator.validInt(value)) {
-        await viewTask();
+        await viewTask(Number(value));
     }
     else {
-        await viewBoard();
+        await viewBoard(String(value));
     }
 };
 
 const viewTimeline = async () => {
     let list = await storage.getTaskList();
     validator.emptyContainer(list);
-
-    let done = 0;
-    let pending = 0;
-    let notes = 0;
+    initialise();
 
     const dates = new Set();
     for(let task of list) {
@@ -53,19 +66,13 @@ const viewTimeline = async () => {
             }
         }
     }
-    let percentCompletion = Math.floor(100*(done/(done + pending)));
-    messages.taskCompleteData(percentCompletion);
-    messages.overView(done, pending, notes);
-    messages.newLine();
+    calculate();
 };
 
 const displayItems = async () => {
     const list = await storage.getTaskList();
     validator.emptyContainer(list);
-
-    let done = 0;
-    let pending = 0;
-    let notes = 0;
+    initialise();
 
     const dashBoards = new Set();
     for(const task of list) {
@@ -93,10 +100,52 @@ const displayItems = async () => {
             }
         }
     }
-    let percentCompletion = Math.floor(100*(done/(done + pending)));
-    messages.taskCompleteData(percentCompletion);
-    messages.overView(done, pending, notes);
-    messages.newLine();
+    calculate();
+};
+
+const viewTask = async (value) => {
+    const list = await storage.getTaskList();
+    initialise();
+
+    for(const item of list) {
+        if(value == item._id) {
+            const task = await makeObject(item);
+            messages.viewTask(task, item._type);
+        }
+        if(item._type === "NOTE") {
+            notes += 1;
+        }
+        else if(item._isComplete === true) {
+            done += 1;
+        }
+        else {
+            pending += 1;
+        }
+    }
+    calculate();
+};
+
+const viewBoard = async (value) => {
+    const list = await storage.getTaskList();
+    initialise();
+
+    messages.boardTitle(value);
+    for(const item of list) {
+        if(value == item._board) {
+            const task = await makeObject(item);
+            messages.viewTask(task, item._type);
+        }
+        if(item._type === "NOTE") {
+            notes += 1;
+        }
+        else if(item._isComplete === true) {
+            done += 1;
+        }
+        else {
+            pending += 1;
+        }
+    }
+    calculate();
 };
 
 const makeObject = async (item) => {
@@ -106,24 +155,6 @@ const makeObject = async (item) => {
         days: getDate.calculateDays(item._date)
     };
     return task;
-};
-
-const viewTask = async (value) => {
-    const list = await storage.getTaskList();
-    for(const task of list) {
-        if(value == task._id) {
-            console.log(task);
-        }
-    }
-};
-
-const viewBoard = async (value) => {
-    const list = await storage.getTaskList();
-    for(const task of list) {
-        if(value == task._board) {
-            console.log(task);
-        }
-    }
 };
 
 
