46e8c45d74c0de493abd353435407cb1f86f6498
anandman03 <anandmansimar@gmail.com>
2020-10-23 14:58:41 +0530

    detailed help message

--- src/helpers/help.js
+++ src/helpers/help.js
@@ -2,25 +2,46 @@
 
 const helpMessage = `
   Usage
-    $ tb [<options> ...]
+    $ tl [<options> ...]
     Options
         none             Display board view
-      --begin, -b        Start/pause task
-      --check, -c        Check/uncheck task
-      --clear            Delete all checked items
-      --delete, -d       Delete item
-      --edit, -e         Edit item description
-      --find, -f         Search for items
-      --help, -h         Display help message
-      --list, -l         List items by attributes
-      --move, -m         Move item between boards
-      --note, -n         Create note
+      --help,     -h     Help log message
+      --version,  -v     Current version
+      --task,     -t     Create task
+      --note,     -n     Create note
+      --remove    -r     Remove items
+      --done,     -d     Mark task done/pending
       --priority, -p     Update priority of task
-      --restore, -r      Restore items from archive
-      --star, -s         Star/unstar item
-      --task, -t         Create task
+      --board     -b     Change board name
+      --edit,     -e     Edit item description
+      --move,     -m     Move item between boards
+      --star,     -s     Star/unstar item
+      --copy,     -c     Copy item description to clipboard
+      --unpin,    -u     Remove all completed tasks
       --timeline, -i     Display timeline view
-      --version, -v      Display installed version
+      --begin,    -b     Begin/Pause task
+      --list,     -l     List items by attributes
+      --erase,    -e     Clear all the items
+
+    Examples
+      $ tl
+      $ tl --help
+      $ tl --version
+      $ tl --task Check commit b:project p:2
+      $ tl --note Complexity of Merge-sort is O(nlogn) b:project
+      $ tl --remove 1
+      $ tl --done 2
+      $ tl --priority 2 p:3
+      $ tl --board c:project n:coding
+      $ tl --edit 2 Some new task description
+      $ tl --move 1 b:college
+      $ tl --star 3
+      $ tl --copy 1 2 3
+      $ tl --unpin
+      $ tl --timeline
+      $ tl --begin 2
+      $ tl --list pending
+      $ tl --erase
 `;
 
 module.exports = helpMessage;
